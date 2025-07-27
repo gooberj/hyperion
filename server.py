db8344124e9739467648688bae03bb83d3a1b031
ACloudyBoy <rueben1509@gmail.com>
2025-07-27 18:13:26 +1000

    main

--- server.py
+++ server.py
@@ -1,16 +1,29 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, request, url_for, render_template, send_from_directory
+from werkzeug.utils import secure_filename
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return render_template('index.html')
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    uploaded_file = request.files['file']
+    if uploaded_file.filename != '':
+        uploaded_file.save('files/' + uploaded_file.filename)
+    return 'Success'
 
-@app.route("/text")
-def hello_world():
-    return 
+@app.route('/')
+def home():
+    return render_template("index.html")
 
-@app.route('/serverfiles/<filename>')
-def download_file(filename):
-    return send_from_directory(directory='serverfiles', path=filename, as_attachment=True)
+@app.route('/downloads/<path:filename>')
+def download_from_directory(filename):
+    directory = 'files/'  # Replace with your directory
+    return send_from_directory(directory, filename, as_attachment=True)
+
+@app.route("/files")
+def files():
+    files = ''
+    for i in os.listdir("files/"):
+        files = files + f"<li><a href="f"downloads/{i}>{i}</a></li>\n"
+    return files
 app.run(debug=True, port=80)
